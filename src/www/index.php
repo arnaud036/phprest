e5455694ff2306c9166cf6cc8c15321a3519965d
Arnaud Delabarre <delabarre.arnaud@gmail.com>
2013-09-21 15:21:05 -0700

    Moved main classes into a lib/ folder

--- src/www/index.php
+++ src/www/index.php
@@ -2,7 +2,7 @@
 
 defined('APP_PATH') || define('APP_PATH', realpath(dirname(__FILE__)));
 
-require_once  APP_PATH . '/../ApiRestServer.php';
+require_once  APP_PATH . '/../lib/ApiRestServer.php';
 
 $api = new ApiRestServer();
 $api->processRequest();
